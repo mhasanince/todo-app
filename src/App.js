5cd8d07630b7c0470d356d0c3a92c26deec6e8a4
mhasanince <mhasanince@gmail.com>
2020-03-10 14:16:57 +0300

    Footer added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import TodoList from './TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <h1>Todo List!</h1>
-        <h4>A Simple React Todo List App.</h4>
+    <div className="container">
+      <div className="App">
+        <div className="App-header">
+          <h1>Todo List!</h1>
+          <h4>A Simple React Todo List App.</h4>
+        </div>
+        <div className="App-content">
+          <TodoList />
+        </div>
       </div>
-      <div className="App-content">
-        <TodoList />
-      </div>
+      <footer>
+        <p>
+          Made with{' '}
+          <span role="img" aria-label="heart">
+            ❤️
+          </span>{' '}
+          for İpek.
+        </p>
+      </footer>
     </div>
   );
 }
